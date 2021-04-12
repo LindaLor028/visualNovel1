837a35528fd07bc05f31bae7a74d1443c79ba125
LindaLor028 <77794383+LindaLor028@users.noreply.github.com>
2021-04-11 21:07:13 -0500

    Implemented JFrame

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,31 @@
 package src;
 
+import java.util.Scanner;
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+
+import java.awt.Color;
+
 public class Test 
 {
     public static void main(String[] args) {
-        System.out.println("Hello! I work");
+        // System.out.println("PLEASE TYPE yes");
+        // Scanner scans = new Scanner(System.in);
+        // String nexttext = scans.next();
+        // if (nexttext.equals("yes")) {
+        //     System.out.println("WORKING");
+        // }
+        // else{
+        //     System.out.println("WRONG INPUT");
+        // }
+
+        JFrame window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.pink);
+        window.setLayout(null);
+        window.setVisible(true);
     }
 
 }
